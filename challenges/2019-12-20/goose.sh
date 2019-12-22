d3563aeeae2177c61986f87ce6911e528fac6b43
Jeffrey Sica <jeef111x@gmail.com>
2019-12-22 14:06:48 -0500

    forgot to add goose.sh

---
+++ challenges/2019-12-20/goose.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+# Thanks to innovia
+# https://gist.github.com/innovia/fbba8259042f71db98ea8d4ad19bd708
+
+SERVICE_ACCOUNT_NAME=goose
+NAMESPACE="lake"
+KUBECFG_FILE_NAME="/tmp/kube/k8s-${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-conf"
+TARGET_FOLDER="/tmp/kube"
+
+create_target_folder() {
+    echo -n "Creating target directory to hold files in ${TARGET_FOLDER}..."
+    mkdir -p "${TARGET_FOLDER}"
+    printf "done"
+}
+
+create_service_account() {
+    echo -e "\\nCreating a service account in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}"
+    kubectl create sa "${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
+}
+
+get_secret_name_from_service_account() {
+    echo -e "\\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
+    SECRET_NAME=$(kubectl get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o json | jq -r .secrets[].name)
+    echo "Secret name: ${SECRET_NAME}"
+}
+
+extract_ca_crt_from_secret() {
+    echo -e -n "\\nExtracting ca.crt from secret..."
+    kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq \
+    -r '.data["ca.crt"]' | base64 -d > "${TARGET_FOLDER}/ca.crt"
+    printf "done"
+}
+
+get_user_token_from_secret() {
+    echo -e -n "\\nGetting user token from secret..."
+    USER_TOKEN=$(kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data["token"]' | base64 -d)
+    printf "done"
+}
+
+set_kube_config_values() {
+    context=$(kubectl config current-context)
+    echo -e "\\nSetting current context to: $context"
+
+    CLUSTER_NAME=$(kubectl config get-contexts "$context" | awk '{print $3}' | tail -n 1)
+    echo "Cluster name: ${CLUSTER_NAME}"
+
+    ENDPOINT=$(kubectl config view \
+    -o jsonpath="{.clusters[?(@.name == \"${CLUSTER_NAME}\")].cluster.server}")
+    echo "Endpoint: ${ENDPOINT}"
+
+    # Set up the config
+    echo -e "\\nPreparing k8s-${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-conf"
+    echo -n "Setting a cluster entry in kubeconfig..."
+    kubectl config set-cluster "${CLUSTER_NAME}" \
+    --server="${ENDPOINT}" \
+    --certificate-authority="${TARGET_FOLDER}/ca.crt" \
+    --embed-certs=true
+
+    echo -n "Setting token credentials entry in kubeconfig..."
+    kubectl config set-credentials \
+    "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
+    --token="${USER_TOKEN}"
+
+    echo -n "Setting a context entry in kubeconfig..."
+    kubectl config set-context \
+    "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
+    --cluster="${CLUSTER_NAME}" \
+    --user="${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
+    --namespace="${NAMESPACE}"
+
+    echo -n "Setting the current-context in the kubeconfig file..."
+    kubectl config use-context "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}"
+}
+
+create_target_folder
+get_secret_name_from_service_account
+extract_ca_crt_from_secret
+get_user_token_from_secret
+set_kube_config_values
